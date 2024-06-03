9d07925fe1f2c549b495908b80c09dbe85c38f72
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-06-03 20:40:29 +0900

    create file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :test, only: %i[index]
+    end
+  end
 end
