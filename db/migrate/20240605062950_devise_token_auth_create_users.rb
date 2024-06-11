188967a9c38f61b30226b7b8738d557fdb60a21c
ftr <fkswtkm0405@gmail.com>
2024-06-11 12:25:26 +0900

    feat: create test api

--- db/migrate/20240605062950_devise_token_auth_create_users.rb
+++ db/migrate/20240605062950_devise_token_auth_create_users.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
   def change
-    
     create_table(:users) do |t|
       ## Required
-      t.string :provider, :null => false, :default => "email"
-      t.string :uid, :null => false, :default => ""
+      t.string :provider, null: false, default: 'email'
+      t.string :uid, null: false, default: ''
 
       ## Database authenticatable
-      t.string :encrypted_password, :null => false, :default => ""
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-      t.boolean  :allow_password_change, :default => false
+      t.boolean  :allow_password_change, default: false
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -29,10 +30,14 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
       # t.datetime :locked_at
 
       ## User Info
-      t.string :name
-      t.string :nickname
-      t.string :image
-      t.string :email
+      t.string :name, null: false, limit: 50
+      t.string :user_name, null: false
+      t.string :email, null: false
+      t.string :phone, null: false
+      t.date :birthdate, null: false
+      t.string :introduction, limit: 160
+      t.string :place, limit: 30
+      t.string :website, limit: 100
 
       ## Tokens
       t.json :tokens
@@ -40,8 +45,9 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, [:uid, :provider],     unique: true
+    add_index :users, :user_name, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, %i[uid provider], unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
