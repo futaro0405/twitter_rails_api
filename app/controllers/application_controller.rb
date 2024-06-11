cc364db5619b943fd38aa8fd825056477b91bdc4
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-06-12 00:13:27 +0900

    feat: ログインアップ画面を更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   skip_before_action :verify_authenticity_token
--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_05_062950) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "name"
-    t.string "nickname"
-    t.string "image"
-    t.string "email"
+    t.string "name", limit: 50, null: false
+    t.string "user_name", null: false
+    t.string "email", null: false
+    t.string "phone", null: false
+    t.date "birthdate", null: false
+    t.string "introduction", limit: 160
+    t.string "place", limit: 30
+    t.string "website", limit: 100
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -37,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_05_062950) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
