66ec97f4efaf2c777e9c067a4868f723aac534ec
ftr <fkswtkm0405@gmail.com>
2024-06-11 15:51:00 +0900

    feat: devise_token_authの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   namespace :api do
     namespace :v1 do
       resources :test, only: %i[index]
@@ -6,6 +8,10 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
       }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
   end
 end
