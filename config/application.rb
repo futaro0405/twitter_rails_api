ae381fbca43d5564e477be980cd11e33e344c866
futaro0405 <fkswtkm0405@gmail.com>
2024-07-08 13:29:52 +0900

    feat: 日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,10 @@ module Myapp
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
     config.generators do |g|
       g.test_framework :rspec,
                        fixtures: false,
